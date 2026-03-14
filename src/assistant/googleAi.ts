85028701572f751e3232c078fc91b888e6de8373
Phan Tu Quoc <quoctup02@gmail.com>
2026-03-14 10:47:31 +0700

    fix api key google

--- src/assistant/googleAi.ts
+++ src/assistant/googleAi.ts
@@ -2,7 +2,7 @@ import {GoogleGenAI} from "@google/genai";
 
 const googleAI = 
        new GoogleGenAI({
-            apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
+            apiKey: import.meta.env.VITE_GOOGLE_API_KEY || process.env.GOOGLE_API_KEY!,
         });
     
 
